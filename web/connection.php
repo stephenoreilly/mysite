a7c9fa9afcbf357c3007b50956cf83655151f14b
Stephen O'Reilly <stiofan.oreilly@gmail.com>
2015-08-17 11:29:10 +0000

    content change and links added

--- web/connection.php
+++ web/connection.php
@@ -7,26 +7,5 @@
     $db = substr($url["path"], 1);
     
     $link = new mysqli($server, $username, $password, $db);
-    print_r($conn);
-//(...)
-if($link == false){
-    echo "failed";
-    //try to reconnect
-}
-else{
-    echo "connected";
-}
 
-  $query="CREATE TABLE users2 (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(30) NOT NULL,
-        email VARCHAR(50),
-        password VARCHAR(50),
-        diary TEXT,
-        reg_date TIMESTAMP
-            )";
-    //    $query="INSERT INTO users2 (name, email, password)
-    //VALUES ('Beth','example@example.com','');";
-
-    mysqli_query($link,$query);
 ?>
