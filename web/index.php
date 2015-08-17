18a8493a176aabab86d390a41f712cbd2acdf473
Stephen O'Reilly <stiofan.oreilly@gmail.com>
2015-08-17 13:42:07 +0000

    fixing bug with connection php file

--- web/index.php
+++ web/index.php
@@ -68,7 +68,7 @@
    	        <div class="topRow col-md-6 col-md-offset-3">
    	            <h1>Welcome</h1>
    	            <h2 class="lead">Scroll down to take a look a some of my recent projects</h2>
-   	            <h3 class="lead">I have been learing different web technologies and am currently looking for a job as a web developer</h3>
+   	            <h3 class="lead">I have been learning different web technologies and am currently looking for a job as a web developer</h3>
    	        </div>
    	    </div>
    		
