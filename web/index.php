103ad5be992ed9d2fbfe8dbafa299c9135fec1ff
Stephen O'Reilly <stiofan.oreilly@gmail.com>
2015-08-17 15:07:22 +0000

    add email and phone

--- web/index.php
+++ web/index.php
@@ -71,7 +71,7 @@
    	        <div class="topRow col-md-6 col-md-offset-3">
    	            <h1>Welcome</h1>
    	            <h2 class="lead">Scroll down to take a look at some of my recent projects</h2>
-   	            <h3 class="lead">I have been learning different web technologies and am currently looking for a job as a web developer</h3>
+   	            <h3 class="lead">I am currently working as a SmallTalk developer for JP Morgan but been learning different web technologies and am currently looking for a job as a web developer</h3>
    	            <h3 class="lead">You can contact me by email <span class="strong">stiofan.oreilly@gmail.com</span> or by phone <span class="strong">+447538539491</span></h3>
    	        </div>
    	    </div>
